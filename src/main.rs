39ea837fc3e5a177b59c8c84f81157c142ab93e4
Maximilian <mpinderwhite@gmail.com>
2024-01-10 16:16:42 +0100

    [+] added argument reader

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::env::args;
+
+fn show_help() {
+    println!("RUST PASSWORD ENCRYPTOR");
+    println!("-h : shows this screen");
+    println!("-e : encrypts a word");
+    println!("-d : decrypts a word");
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut args = args();
+    let mut _program = args.next().expect("program");
+    if let Some(argument) = args.next() {
+        println!("{}", argument);
+        match argument.as_str() {
+            "-e" => println!("sussy baka"),
+            "-d" => println!("sussD baka"),
+            _ => show_help(),
+        }
+    }
 }
