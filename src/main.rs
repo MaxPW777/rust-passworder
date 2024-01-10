53283d602b7b68807490498d095022f52a60c276
Maximilian <mpinderwhite@gmail.com>
2024-01-10 16:34:01 +0100

    [+] split function

--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,30 @@ fn show_help() {
     println!("-d : decrypts a word");
 }
 
+fn encrypt_password(word: &str) -> String {
+    String::from(word)
+}
+fn decrypt_password(word: &str, key: &str) -> String {
+    String::from(word)
+}
+
 fn main() {
     let mut args = args();
     let mut _program = args.next().expect("program");
     if let Some(argument) = args.next() {
-        println!("{}", argument);
         match argument.as_str() {
-            "-e" => println!("sussy baka"),
-            "-d" => println!("sussD baka"),
+            "-e" => {
+                let word = args.next().expect("no input received");
+                println!("{}", encrypt_password(&word));
+            }
+            "-d" => {
+                let word = args.next().expect("no input received");
+                let key = args.next().expect("no key received");
+                println!("{}", decrypt_password(&word, &key));
+            }
             _ => show_help(),
         }
+    } else {
+        show_help()
     }
 }
